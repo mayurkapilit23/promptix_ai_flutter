df3d24be9f9d82cafcfb8d20ffa539aa5700d539
mayurkapitit23 <mayurp@kapilit.com>
2025-12-26 18:57:06 +0530

    UI: implemented splash screen ui

---
+++ lib/features/onboarding/bloc/splash_state.dart
@@ -0,0 +1,11 @@
+
+
+
+abstract class SplashState {}
+
+class SplashInitial extends SplashState {}
+
+class SplashNavigate extends SplashState {
+  final String route;
+  SplashNavigate(this.route);
+}
---
+++ lib/features/onboarding/views/splash_screen.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:promptix_ai_flutter/core/utils/app_routes.dart';
+
+import '../bloc/splash_bloc.dart';
+import '../bloc/splash_state.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocListener<SplashBloc, SplashState>(
+      listener: (context, state) {
+        if (state is SplashNavigate) {
+          Navigator.pushNamed(context, AppRoutes.login);
+        }
+      },
+      child: Scaffold(
+        body: Center(
+          child: TweenAnimationBuilder<double>(
+              tween: Tween(begin: 0, end: 1),
+              duration: const Duration(milliseconds: 1200),
+              curve: Curves.easeOut,
+            builder: (context, value, child) {
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Opacity(
+                    opacity: value,
+                    child: Transform.scale(
+                      scale: 0.8 + (0.2 * value),
+                      child: Image.asset(
+                        'assets/pyramid.png',
+                        height: 80,
+                        width: 80,
+                      ),
+                    ),
+                  ),
+
+                  const SizedBox(height: 12),
+
+                  Opacity(
+                    opacity: value,
+                    child: Transform.translate(
+                      offset: Offset(0, 20 * (1 - value)),
+                      child: const Text(
+                        'Promptix Ai',
+                        style: TextStyle(
+                          fontSize: 22,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              );
+            }
+          ),
+        ),
+      ),
+    );
+  }
+}
